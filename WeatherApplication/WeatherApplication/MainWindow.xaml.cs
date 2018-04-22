aad5bb2503b5150cf62be78f2d1d6d1facd28cdf
iania <stefaniadenisaionescu@users.noreply.github.com>
2018-04-22 14:17:47 -0700

    Add files via upload

---
+++ WeatherApplication/WeatherApplication/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WeatherApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void CheckButton_Click(object sender, RoutedEventArgs e)
+        {
+            var temp = await WeatherHelper.init(CityTextBox.Text);
+
+            var source = WeatherHelper.GetImage();
+            Image.Source = source;
+
+            double degrees = WeatherHelper.GetTemperature();
+            ResultTextBox.Text = $"{Math.Round(degrees)} °C";
+
+            string city = CityTextBox.Text;
+            string country = WeatherHelper.GetCountry();
+            CityNameTextBox.Text = city + ", " + country;
+
+            string description = WeatherHelper.GetDescription();
+            DescriptionTextBox.Text = description;
+
+            string time = WeatherHelper.GetTime();
+            TimeTextBox.Text = time;
+
+            double humidity = WeatherHelper.GetHumidity();
+            HumidityTextBox.Text = $"{humidity} %";
+
+            double pressure = WeatherHelper.GetPressure();
+            PressureTextBox.Text = $"{pressure} hpa";
+
+            double windspeed = WeatherHelper.GetWindSpeed();
+            WindSpeedTextBox.Text = $"{windspeed} m/s";
+        }
+    }
+}
---
+++ WeatherApplication/WeatherApplication/WeatherHelper.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace WeatherApplication
+{
+    internal static class WeatherHelper
+    {
+        private static string AppId = "5a9f5e7ae554f4156e5442293048f920";
+        private static WeatherResult response;
+
+        internal static async Task<bool> init(string cityName)
+        {
+            var httpClient = new HttpClient();
+            var requestUri = $"http://api.openweathermap.org/data/2.5/forecast?q={cityName}&appid={AppId}";
+            var json = await httpClient.GetStringAsync(requestUri);
+
+            response = JsonConvert.DeserializeObject<WeatherResult>(json);
+
+            return true; // async void methods are not okay
+        }
+
+        internal static BitmapImage GetImage()
+        {
+            var icon = $"https://openweathermap.org/img/w/{response.list[0].weather[0].icon}.png";
+            var source = new BitmapImage(new Uri(icon));
+
+            return source;
+        }
+
+        internal static double GetTemperature()
+        {
+            return ConvertKelvinToCelciusDegrees(response.list[0].main.temp);
+        }
+
+        internal static double GetHumidity()
+        {
+            return response.list[0].main.humidity;
+        }
+
+        internal static double GetPressure()
+        {
+            return response.list[0].main.pressure;
+        }
+
+        internal static double GetWindSpeed()
+        {
+            return response.list[0].wind.speed;
+        }
+
+        internal static string GetCountry()
+        {
+            return response.city.country;
+        }
+
+        internal static string GetDescription()
+        {
+            return response.list[0].weather[0].description;
+        }
+
+        internal static string GetTime()
+        {
+            return response.list[0].dt_txt;
+        }
+
+        private static double ConvertKelvinToCelciusDegrees(double kelvin)
+        {
+            return kelvin - 273.15;
+        }
+    }
+}
